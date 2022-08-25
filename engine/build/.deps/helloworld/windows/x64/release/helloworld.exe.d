1c11af69c2aa32f1e29651377e2796ee0c97fc71
Andrew <michael.a.williams@mawfia.com>
2022-08-24 20:41:00 -0400

    first checkpoint

---
+++ engine/build/.deps/helloworld/windows/x64/release/helloworld.exe.d
@@ -0,0 +1,16 @@
+{
+    files = {
+        [[build\.objs\helloworld\windows\x64\release\demo\helloworld.cpp.obj]]
+    },
+    values = {
+        [[C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Tools\MSVC\14.33.31629\bin\HostX64\x64\link.exe]],
+        {
+            "-nologo",
+            "-dynamicbase",
+            "-nxcompat",
+            "-machine:x64",
+            "/opt:ref",
+            "/opt:icf"
+        }
+    }
+}
