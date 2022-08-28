691b512b6ba59cfbbadf0f2b2356beeec82b7188
Andrew <michael.a.williams@mawfia.com>
2022-08-28 16:18:53 -0400

    Updated checkpoin2 completion

--- demo/helloworld.cpp
+++ demo/helloworld.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "Engine.h"
+
+using namespace std;
+
+void function1() {
+    cout << "initialize callback" << '\n';
+}
+
+void function2() {
+    cout << "update callback" << '\n';
+}
 
 int main( int argc, const char* argv[] ) {
-    std::cout << "Hello, World!\n";
+
+    using namespace engine;
+
+    Engine e;
+    int GameParameters = 0;
+    e.Start(GameParameters, function1, function2);
+    e.name = "Engine started!";
+    cout << e.name << '\n';
+
+
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,21 @@
 add_rules("mode.debug", "mode.release")
+add_requires("glfw")
 
-target("helloworld")
-    set_kind("binary")
-    set_languages("cxx17")
+  target("engine")
+      set_kind("static")
+      set_languages("cxx17")
 
-    add_files("demo/helloworld.cpp")
+      -- Declare our engine's header path.
+      -- This allows targets that depend on the engine to #include them.
+      add_includedirs("src", {public = true})
+
+      add_files("src/*.cpp")
+      add_packages("glfw")
+
+  target("helloworld")
+      set_kind("binary")
+      set_languages("cxx17")
+
+      add_deps("engine")
+
+      add_files("demo/helloworld.cpp")
