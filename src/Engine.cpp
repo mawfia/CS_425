75316266247739a069e99606cf73e99908a6a6e9
Andrew <michael.a.williams@mawfia.com>
2022-09-02 12:55:23 -0400

    Added 60 FPS code

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -2,6 +2,7 @@
 
 #include <thread>
 #include <chrono>
+#include <iostream>
 
 #include "Engine.h"
 
@@ -15,18 +16,25 @@ namespace engine {
 
 	void Engine::Start(int GameParameters, void (*SetupCallback)(), void (*UpdateCallback)()) {
 		
-		const auto one_tenth_of_a_second = chrono::duration<double>(1. / 10.);
+		const auto dt = chrono::duration<double>(1. / 60.);
+		
 
 		this->Startup(); // Engine::Startup() will work as well
 		SetupCallback();
-
+		
+		int x = 0;
 		
 		 while( true ){
-
+			 auto currentTime = chrono::steady_clock::now();
 			//UpdateCallback();
 
 			//this->graphics.Draw();
-			this_thread::sleep_for(one_tenth_of_a_second);
+
+			 auto newTime = chrono::steady_clock::now();
+			 auto loopTime = newTime - currentTime;
+			 auto delta = dt - loopTime;
+			
+			 if (delta.count() > 0) this_thread::sleep_for(delta);
 		 }
 		
 	}
